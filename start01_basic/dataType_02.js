80a09e5aefc9b0de843c57165c9f9be4403d247f
khandvemukesh <76439454+khandvemukesh@users.noreply.github.com>
2023-10-12 20:09:51 +0000

    dataType Basic javascript

---
+++ start01_basic/dataType_02.js
@@ -0,0 +1,32 @@
+"use strict" // Treat all js code in the new version
+
+// alert(3+3) alert is not defined 
+ // we are use nodejs not use browser
+ console.log(3+3)
+ console.log('mukesh') // code readablity  in so high
+
+const dType='Mukesh'
+let age=25
+
+let isLoggedIn=false
+let state;
+ 
+//number =>2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standelone value
+//undefined=>
+//symbol => unique
+
+//object
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+
+
+
+
+
+
+

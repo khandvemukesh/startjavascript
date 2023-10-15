60e301796c62358c3b8c5eacf0075a5a3a3c9b54
khandvemukesh <76439454+khandvemukesh@users.noreply.github.com>
2023-10-15 10:47:40 +0000

    math and Date Function

---
+++ start01_basic/data_math_01.js
@@ -0,0 +1,14 @@
+// Math Function ******************
+//console.log(Math); //math is a object
+//console.log(Math.abs(-4)); //Absolulty Number
+//console.log(Math.round(5.6)); // Round the value of
+//console.log(Math.ceil(4.6)); // a
+// console.log(Math.floor(4.2));
+// console.log(Math.min(4,5,2,6));
+// console.log(Math.max(2,7,8,9));
+
+console.log(Math.floor(Math.random()*6)+1);
+
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random()*(max-min + 1))+min);
---
+++ start01_basic/date_example.js
@@ -0,0 +1,26 @@
+let myDate=new Date()
+// console.log(myDate.toString());
+// console.log(myDate.getDate());
+// console.log(myDate.getMonth());
+// console.log(myDate.getFullYear());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.myDate);
+
+
+// let myCreatedDate=new Date(2023 ,12, 23)
+//let myCreatedDate=new Date(2023 ,12, 23,5,3)
+// let myCreatedDate=new Date("2023-10-14")
+// let myCreatedDate=new Date("01-14-2023")
+// console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDate.toLocaleString());
+
+// let myTimestamp=Date.now()
+// console.log(myTimestamp);
+
+let newDate =new Date()
+console.log(newDate)
+newDate.toLocaleString('default', 
+{weekday:"long",
+})
+console.log(newDate);

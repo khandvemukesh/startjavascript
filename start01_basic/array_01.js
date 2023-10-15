2ce9704ed664495039fe849021597916a0b2f572
khandvemukesh <76439454+khandvemukesh@users.noreply.github.com>
2023-10-15 18:09:40 +0000

    array

---
+++ start01_basic/array_01.js
@@ -0,0 +1,44 @@
+//array
+const myArr=[0,1,3,4,5]
+// const Heros=['Junior ji', 'Shaktiman']
+
+// console.log(myArr.length);
+// console.log(Heros);
+
+//Array Methods
+// myArr.push(6)
+// myArr.push(7) //push() use for add element in the array 
+// myArr.pop()   //pop() use for delete last element in the array 
+ //myArr.unshift(9) //
+//  console.log(myArr.unshift(9))
+//  console.log(myArr.shift(7))
+//  console.log(myArr)
+ //console.log(myArr.shift(5))
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr=myArr.join();
+// console.log(myArr);
+// console.log(newArr);
+
+//slice ,splice
+console.log(myArr)
+console.log('A ',myArr);
+const myn1=myArr.slice(1, 3, 4)
+console.log(myn1);
+console.log('B ',myArr);
+const myn2=myArr.splice(1, 3, 4)
+console.log('c ',myArr);
+console.log(myn2);
+
+
+// console.log(Array.isArray("Hitesh"))
+
+//console.log(Array.from("Hitesh")) // Array.from() is use for making   
+//console.log(Array.from({Name: "Hitesh"})) //interesting
+let score=200;
+let score1=200;
+let score2=200;
+
+//console.log(Array.of(score, score1, score2));// Array.of() is use for making   
+
